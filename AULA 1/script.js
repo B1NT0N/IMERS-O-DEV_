3efdc7bdb34ca3b9dbdab62c43cdf47a678b33ef
Kleiton Andrade <kleiton1002andrade@gmail.com>
2021-09-14 01:30:12 -0100

    First lesson done

--- AULA 1/script.js
+++
@@ -1,3 +0,0 @@
-
-var nota = 10;
-console.log(nota)
---
+++ Average_calculator/script.js
@@ -0,0 +1,11 @@
+
+var name = "B!N" // Defining a string 
+
+var firstGrade = 9; // Defining a int/float 
+var secondGrade = 5;
+var thirdGrade = 4;
+var fourthGrade = 3;
+
+var finalGrade = ((firstGrade + secondGrade + thirdGrade + fourthGrade) / 4).toFixed(1); // Calculating the average grade and rounding to .1f 
+
+console.log(finalGrade);
