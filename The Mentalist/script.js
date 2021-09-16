221a64b65b39784cc449b806d77a88db49af4a22
Kleiton Andrade <kleiton1002andrade@gmail.com>
2021-09-15 23:29:15 -0100

    Lesson 2 custom

--- The Mentalist/script.js
+++ The Mentalist/script.js
@@ -5,10 +5,10 @@ var random = parseInt(Math.random()*11);
 function Chutar() {
     var element = document.getElementById("resultado");
     var guess = parseInt(document.getElementById("valor").value);
-    var output = " "
+    var output = " ";
     console.log(guess);
     if (guess == random) {
-        output = "CONGARTULATIONS the number is " + random ;
+        output = "CONGARTULATIONS the number is " + random;
         random = parseInt(Math.random()*11);
     } else if (guess > 10 || guess < 0) {
         output = "Number must be between 0 and 10";
