5c7ba55bd5972a2b49da9f3f8c92c4af682c1ff5
Kleiton Andrade <kleiton1002andrade@gmail.com>
2021-09-16 17:28:23 -0100

    Lesson 4 Done

--- HackFlix/script.js
+++ HackFlix/script.js
@@ -1,7 +1,12 @@
-var filme1 = "AKA";
-var filme2 = "OPO";
-var filme3 = "ASS";
+var movie = [
+    "https://m.media-amazon.com/images/M/MV5BOGYwYTA5M2QtMTk3Zi00ZjdjLWFkNDUtYzg4MjM0ZGI0MGU1XkEyXkFqcGdeQXVyODIyOTEyMzY@._V1_.jpg",
+    "https://m.media-amazon.com/images/M/MV5BMmE1ODVhMGYtODYyYS00Mjc4LWIzN2EtYWZkZDg1MTUyNDkxXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
+    "https://m.media-amazon.com/images/M/MV5BMjA5OTNhNDQtODU0ZS00MmMyLWJkZmMtZmIyNjRhOTFlODMwXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_FMjpg_UX1000_.jpg",
+    "https://m.media-amazon.com/images/M/MV5BZjRjOTFkOTktZWUzMi00YzMyLThkMmYtMjEwNmQyNzliYTNmXkEyXkFqcGdeQXVyNzQ1ODk3MTQ@._V1_.jpg",
+    "https://m.media-amazon.com/images/M/MV5BMTc4MDQwOTE4N15BMl5BanBnXkFtZTgwODgzMDQxNjE@._V1_.jpg",
+];
 
-var filme = [filme1,filme2,filme3];
+for (var i = 0; i < movie.length; i++) {
+    document.write("<img src="+movie[i] + ">");
+}
 
-document.write("<p>"+ filme1 + "</p>");
