02f67f826ea0e1eab842e902f8e62e16572502aa
Kleiton Andrade <kleiton1002andrade@gmail.com>
2021-09-15 23:24:10 -0100

    Lesson 2 Done

--- The Mentalist/script.js
+++ The Mentalist/script.js
@@ -1,17 +1,24 @@
-var random = 0;
+var random = parseInt(Math.random()*11);
+
+
 
 function Chutar() {
+    var element = document.getElementById("resultado");
     var guess = parseInt(document.getElementById("valor").value);
+    var output = " "
     console.log(guess);
     if (guess == random) {
-        console.log("Acertou")
+        output = "CONGARTULATIONS the number is " + random ;
+        random = parseInt(Math.random()*11);
+    } else if (guess > 10 || guess < 0) {
+        output = "Number must be between 0 and 10";
     } else {
 
         if (guess > random) {
-            console.log("Menor")
+            output = "Hint: The number is SMALLER";
         }else{
-            console.log("Maior")
-        }
-        
+            output = "Hint: The number is BIGGER";
+        }  
     }
+    element.innerHTML = output;
 }
