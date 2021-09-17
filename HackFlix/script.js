e48870478ad3179b09ce952eb5cfa8683c34200c
Kleiton Andrade <kleiton1002andrade@gmail.com>
2021-09-17 17:18:56 -0100

    Lesson 5 Start

--- HackFlix/script.js
+++ HackFlix/script.js
@@ -1,12 +1,24 @@
-var movie = [
+/* var movie = [
     "https://m.media-amazon.com/images/M/MV5BOGYwYTA5M2QtMTk3Zi00ZjdjLWFkNDUtYzg4MjM0ZGI0MGU1XkEyXkFqcGdeQXVyODIyOTEyMzY@._V1_.jpg",
     "https://m.media-amazon.com/images/M/MV5BMmE1ODVhMGYtODYyYS00Mjc4LWIzN2EtYWZkZDg1MTUyNDkxXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
     "https://m.media-amazon.com/images/M/MV5BMjA5OTNhNDQtODU0ZS00MmMyLWJkZmMtZmIyNjRhOTFlODMwXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_FMjpg_UX1000_.jpg",
     "https://m.media-amazon.com/images/M/MV5BZjRjOTFkOTktZWUzMi00YzMyLThkMmYtMjEwNmQyNzliYTNmXkEyXkFqcGdeQXVyNzQ1ODk3MTQ@._V1_.jpg",
     "https://m.media-amazon.com/images/M/MV5BMTc4MDQwOTE4N15BMl5BanBnXkFtZTgwODgzMDQxNjE@._V1_.jpg",
-];
+]; */
+
+
 
 for (var i = 0; i < movie.length; i++) {
     document.write("<img src="+movie[i] + ">");
 }
 
+function adicionarFilme(){
+    var id = document.getElementById("filme").value;
+    var tagId = "<img src="+id + ">";
+    var tag = document.getElementById("listaFilmes");
+    tag.innerHTML += tagId;
+
+    document.getElementById("filme").value = " "
+
+    //console.log(movie)
+}
