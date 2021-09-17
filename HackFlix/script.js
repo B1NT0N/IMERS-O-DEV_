4a88f0ac29f49457455e26b5ea7ee591d0335bad
Kleiton Andrade <kleiton1002andrade@gmail.com>
2021-09-17 17:41:40 -0100

    Lesson 5 Done

--- HackFlix/script.js
+++ HackFlix/script.js
@@ -4,21 +4,28 @@
     "https://m.media-amazon.com/images/M/MV5BMjA5OTNhNDQtODU0ZS00MmMyLWJkZmMtZmIyNjRhOTFlODMwXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_FMjpg_UX1000_.jpg",
     "https://m.media-amazon.com/images/M/MV5BZjRjOTFkOTktZWUzMi00YzMyLThkMmYtMjEwNmQyNzliYTNmXkEyXkFqcGdeQXVyNzQ1ODk3MTQ@._V1_.jpg",
     "https://m.media-amazon.com/images/M/MV5BMTc4MDQwOTE4N15BMl5BanBnXkFtZTgwODgzMDQxNjE@._V1_.jpg",
-]; */
+]; 
 
 
 
 for (var i = 0; i < movie.length; i++) {
     document.write("<img src="+movie[i] + ">");
-}
+}*/
 
 function adicionarFilme(){
     var id = document.getElementById("filme").value;
+    if  (id.endsWith(".jpg") || id.endsWith(".png")){
+    showOnScreen(id)
+
+    }else{
+        console.error("INVALID URL")
+    }
+    document.getElementById("filme").value = " "
+    //console.log(movie)
+}
+
+function showOnScreen(id){
     var tagId = "<img src="+id + ">";
     var tag = document.getElementById("listaFilmes");
     tag.innerHTML += tagId;
-
-    document.getElementById("filme").value = " "
-
-    //console.log(movie)
 }
