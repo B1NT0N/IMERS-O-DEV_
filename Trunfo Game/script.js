35a8b833a3d39910febf155b7d13f829393e6f4a
Kleiton Andrade <kleiton1002andrade@gmail.com>
2021-09-21 18:15:58 -0100

    Lsson 7 Beginning

---
+++ Trunfo Game/script.js
@@ -0,0 +1,34 @@
+var card1 = {
+    name:"Niles Vanish",
+    specs: {
+        atk:0,
+        def:0,
+        mgc:10,
+        
+    }
+}
+var card2 = {
+    name:"Okay",
+    specs: {
+        atk:10,
+        def:0,
+        mgc:0,
+        
+    }
+}
+var card3 = {
+    name:"WTF",
+    specs: {
+        atk:0,
+        def:10,
+        mgc:0,
+        
+    }
+}
+var cards = [card1, card2, card3]
+var computer
+var player
+
+function sortearCarta(){
+
+}
