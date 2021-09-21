5d6d48ae8cfd54aa7085af989a2bcfd28d42ff9b
Kleiton Andrade <kleiton1002andrade@gmail.com>
2021-09-21 19:35:58 -0100

    Lesson 7 Done

--- Trunfo Game/script.js
+++ Trunfo Game/script.js
@@ -30,5 +30,49 @@ var computer
 var player
 
 function sortearCarta(){
+    var randComputer = parseInt(Math.random()*3);
+    var randPlayer = parseInt(Math.random()*3);
 
+    computer = cards[randComputer];
+    player = cards[randPlayer];
+    
+    document.getElementById("btnSortear").disabled = true;
+    document.getElementById("btnJogar").disabled = false;
+
+    showOptions()
 }
+
+function showOptions(){
+    var tag = document.getElementById("opcoes");
+    var options = "";
+    for (var spec in player.specs) {
+        options += "<input type = 'radio' name = 'spec' value = '"+ spec +"'>" + spec;
+
+    }
+    
+    tag.innerHTML = options;
+}
+
+function getOptions(){
+    var options = document.getElementsByName("spec");
+    for (var i = 0; i < options.length; i++) {
+        if (options[i].checked == true) {
+            return options[i].value;
+        }
+    }
+    
+}
+function jogar(){
+    var option  = getOptions();
+    var tag = document.getElementById("resultado");
+    if (player.specs[option] > computer.specs[option]){
+        tag.innerHTML = "Yow Won"
+    }else if(player.specs[option] < computer.specs[option]){
+        tag.innerHTML = "Yow Lost"
+    }else{
+        tag.innerHTML = "Draw"
+    }
+    document.getElementById("btnSortear").disabled = false;
+    document.getElementById("btnJogar").disabled = true;
+}
+
