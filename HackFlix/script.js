77cbd564f423a2f7852f8a7a05a72f8200b0a8ea
Kleiton Andrade <kleiton1002andrade@gmail.com>
2021-10-24 12:23:01 -0100

    Readme Update

--- HackFlix/script.js
+++ HackFlix/script.js
@@ -1,4 +1,4 @@
-/* var movie = [
+ var movie = [
     "https://m.media-amazon.com/images/M/MV5BOGYwYTA5M2QtMTk3Zi00ZjdjLWFkNDUtYzg4MjM0ZGI0MGU1XkEyXkFqcGdeQXVyODIyOTEyMzY@._V1_.jpg",
     "https://m.media-amazon.com/images/M/MV5BMmE1ODVhMGYtODYyYS00Mjc4LWIzN2EtYWZkZDg1MTUyNDkxXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
     "https://m.media-amazon.com/images/M/MV5BMjA5OTNhNDQtODU0ZS00MmMyLWJkZmMtZmIyNjRhOTFlODMwXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_FMjpg_UX1000_.jpg",
@@ -7,25 +7,24 @@
 ]; 
 
 
-
 for (var i = 0; i < movie.length; i++) {
     document.write("<img src="+movie[i] + ">");
-}*/
-
-function adicionarFilme(){
-    var id = document.getElementById("filme").value;
-    if  (id.endsWith(".jpg") || id.endsWith(".png")){
-    showOnScreen(id)
-
-    }else{
-        console.error("INVALID URL")
-    }
-    document.getElementById("filme").value = " "
-    //console.log(movie)
 }
 
-function showOnScreen(id){
-    var tagId = "<img src="+id + ">";
-    var tag = document.getElementById("listaFilmes");
-    tag.innerHTML += tagId;
-}
+// function adicionarFilme(){
+//     var id = document.getElementById("filme").value;
+//     if  (id.endsWith(".jpg") || id.endsWith(".png")){
+//     showOnScreen(id)
+
+//     }else{
+//         console.error("INVALID URL")
+//     }
+//     document.getElementById("filme").value = " "
+//     //console.log(movie)
+// }
+
+// function showOnScreen(id){
+//     var tagId = "<img src="+id + ">";
+//     var tag = document.getElementById("listaFilmes");
+//     tag.innerHTML += tagId;
+// }
